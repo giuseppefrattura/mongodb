a520b68974932ad1a32ab65e491ecabe23c010d5
Giuseppe Frattura <giuseppefrattura@gmail.com>
2021-02-02 19:44:20 +0100

    first commit

---
+++ src/test/java/com/giuseppefrattura/mongodb/MongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giuseppefrattura.mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
